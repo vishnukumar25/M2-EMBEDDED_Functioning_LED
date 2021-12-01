e70950a5a6b9c7c766071f5d61dce3a43fad7993
vishnukumar25 <88477015+vishnukumar25@users.noreply.github.com>
2021-12-01 16:19:40 +0530

    Update switch_on.c

--- 3.Implementation/src/switch_on.c
+++ 3.Implementation/src/switch_on.c
@@ -1,1 +1,21 @@
 
+#include "switch.h"
+
+#define F_CPU 1000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+
+void switch_on()
+{
+    PORTB |=(1<<PORTB2);
+		PORTB &=~(1<<PORTB3);
+		_delay_ms(2000);
+		
+		PORTB &=~(1<<PORTB2);
+		_delay_ms(4000);
+		PORTB |=(1<<PORTB3);
+		_delay_ms(2000);
+
+}
